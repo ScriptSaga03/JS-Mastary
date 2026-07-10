7d6e5e7830dcc30bece25aed511d2de364ed635f
Mehtab <mehtab185198131@gmail.com>
2026-07-10 13:55:17 +0530

    Update core_methods.js

--- 01_array_basics/core_methods.js
+++ 01_array_basics/core_methods.js
@@ -1,3 +1,85 @@
+// create an array
+let team = ["Kohli", "Rohit", "Bumrah"];
+console.log(team)
+
+/*
+read value  / access value
+Array se value nikalne ke liye hum square brackets [] ke andar uska index number likhte hain.
+*/
+
+console.log(team[0]);  // output kohli (first player)
+console.log(team[2]);  // output bumrah [last player]
+
+// output undefiend :  agr esi index maang li jo hai hi nhi to undefiend
+console.log(team[5])
+
+/*
+2. Update / Re-assign (Value badalna)
+Kisi specific index par jaakar hum purani value ko nayi value se replace kar sakte hain.
+*/
+
+team[1] = "Dhoni"
+console.log(team)
+
+
+
+// create / insert value
+
+// aakhir mai value dalna ho to 
+team.push('Ronaldo');
+
+
+// shuruat mai value dalni ho to
+team.unshift('Yamal');
+console.log(team)
+
+
+// delete value nikalna ya hatana ho 
+// pop : last element remove kr deta and return bhi krta hai 
+let removePlayer =  team.pop();
+console.log(team);
+console.log(removePlayer)
+
+
+// starting se nikalna ho to shift 
+let removeFirstPlayer = team.shift();
+console.log(team);
+console.log(removeFirstPlayer)
+
+
+
+// Task
+let fruits = ["Apple", "Banana"];
+const addFruitsInLast = (list, item ) =>{
+    list.push(item);
+    return list;
+}
+
+console.log('add item :' , addFruitsInLast(fruits, "Grapes"));
+
+console.log('add item :' , addFruitsInLast(fruits, "Watermelon"));
+
+
+
+// add fruits in starting
+const addFruitsInFirst = (list, item )  => {
+     list.unshift(item);
+     return list
+}
+
+
+console.log('add item : ', addFruitsInFirst(fruits, "strawberry" ));
+
+
+// update item 
+const updateFruitList = (item , index, value) =>{
+    item[index] = value;
+    return item
+}
+
+console.log('update fruit list: ', updateFruitList(fruits, 3,"ice-cream"));
+
+
 
 // build in method
 
@@ -96,6 +178,7 @@ console.log('chunk :', getChuck(techStack, 0,2))
 
 
 
+/*
 5. join() (Array ko Text/String banana)
 Yeh array ke saare elements ko aapas mein jod kar ek single string (text) bana deta hai. 
 Aap brackets ke andar bata sakte ho ki beech mein kya lagana hai (jaise comma, space, ya dash)
@@ -150,3 +233,30 @@ console.log(repeatList.lastIndexOf("Aloo")); // Output: 3
 
 // Jo cheez hai hi nahi:
 console.log(repeatList.indexOf("Chicken"));
+
+
+
+let rawSkills = ["Git", "HTML", "CSS", "Docker", "HTML"];
+
+const findLastIndex = (arr, item) => {
+    return arr.lastIndexOf(item)
+}
+
+console.log('find html last index :' , findLastIndex(rawSkills, 'HTML'));
+
+const sortAndModifiedList = (arr) =>{
+   return  [...arr].sort().join("-")
+}
+console.log('sort and modified list :', sortAndModifiedList(rawSkills));
+const isExistSkill = (arr, item) => {
+    let index = arr.indexOf(item);
+    
+    if (index > -1) {
+        return `Skill found at index: ${index}`;
+    } else {
+        return "Skill not found!";
+    }
+}
+
+console.log(isExistSkill(rawSkills, "React"));  // Output: Skill not found!
+console.log(isExistSkill(rawSkills, "Docker"));
